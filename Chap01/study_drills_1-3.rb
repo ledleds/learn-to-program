fbbdd2805f5eec04b04f0ce5ecb963276404bf41
Vicky Ledsom <victorialedsom@gmail.com>
2017-01-24 14:46:25 +0000

    Chapter 1

---
+++ Chap01/study_drills_1-3.rb
@@ -0,0 +1,11 @@
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+
+#The # sign comments a line out. 
+
+puts "This is my new line!"
