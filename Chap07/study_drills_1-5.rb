28b4ec9d76e624c75dad72aa99123370c3db517a
Vicky Ledsom <victorialedsom@gmail.com>
2017-01-24 19:31:53 +0000

    Chapter 7

---
+++ Chap07/study_drills_1-5.rb
@@ -0,0 +1,26 @@
+# puts out a string to the screen.
+puts "Mary had a little lamb."
+# puts out a string to the screen and passes a string in through interpolation.
+puts "Its fleece was white as #{'snow'}."
+# puts out a strint to the screen.
+puts "And everywhere that Mary went."
+# puts out the string '.' 10 times.
+puts "." * 10  # what'd that do?
+# Creates lots of variables and sets strings.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Print prints out on the same line. This one prints out all of the strings from the variables that are called.
+print end1 + end2 + end3 + end4 + end5 + end6
+# Puts appends a new line.
+puts end7 + end8 + end9 + end10 + end11 + end12
