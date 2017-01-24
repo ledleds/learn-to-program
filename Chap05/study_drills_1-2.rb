a8aa0575c1609bd10ea15e7d9728b8f742e8921b
Vicky Ledsom <victorialedsom@gmail.com>
2017-01-24 18:00:03 +0000

    Chapter 5

---
+++ Chap05/study_drills_1-2.rb
@@ -0,0 +1,18 @@
+name = 'Zed A. Shaw'
+age = 35 #not a lie in 2009
+height = 74
+height_cm = height * 2.54 #centimeters
+weight = 180
+weight_kgs = weight * 0.45359237 #kgs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Lets talk about #{name}."
+puts "He's #{height_cm} centimeters tall."
+puts "He's #{weight_kgs} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight}. I get #{age + height + weight}."
