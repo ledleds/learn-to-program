ca13d9bd56f8d12b9cc1e9b40247fd64637d5f28
Vicky Ledsom <victorialedsom@gmail.com>
2017-01-27 10:07:05 +0000

    Chapter 29

---
+++ Chap29/study_drills.rb
@@ -0,0 +1,50 @@
+people = 20
+cats = 15
+dogs = 15
+
+if people != cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people == cats
+  puts "Not many cats, the world is saved!"
+end
+
+if people <= dogs
+  puts "The world is drooled on!"
+end
+
+if (people && dogs) > (people && cats)
+  puts "The world is dry!"
+end
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greater or equal to dogs"
+end
+
+if people <= dogs
+  puts "People are less than or equal to dogs"
+end
+
+if people == dogs
+  puts "People are dogs"
+end
+
+# Study Drills
+
+# Q: What do you think the if does to the code under it?
+# A: The if-statement will run the code below if the condition emits to true.
+
+# Q: Why does the code under the if need to be indented two spaces?
+# A: For better readability.
+
+# Q: What happens if it isn't indented?
+# A: Nothing, you would just be writing ugly code.
+
+# Can you put other boolean expressions from Exercise 27 in the if-statement? Try it.
+# Changed above.
+
+# Q: What happens if you change the initial values for people, cats, and dogs?
+# A: You would have to change all the values in the program. Otherwise your code wouldn't work & would come back with an error.
