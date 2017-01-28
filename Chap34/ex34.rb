dc693e65cb721837adfda3833011ea5bcbae4340
Vicky Ledsom <victorialedsom@gmail.com>
2017-01-28 15:57:43 +0000

    Chapter 34

---
+++ Chap34/ex34.rb
@@ -0,0 +1,25 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+# The animal at 1.
+puts  "The animal at 1 is the second 'animal' and is 'ruby'"
+puts animals[1]
+# The third (3rd) animal.
+puts "The 3rd animal is at 2 and is 'peacock'"
+puts animals[2]
+# The first (1st) animal.
+puts "The 1st animal is at 0 and is 'bear'"
+puts animals[0]
+# The animal at 3.
+puts "The animal at 3 is the 4th animal and is'kangaroo"
+puts animals[3]
+# The fifth (5th) animal.
+puts "The 5th animal is at 4 and is 'whale'"
+puts animals[4]
+# The animal at 2.
+puts "The animal at 2 is the third animal and is 'peacock'"
+puts animals[2]
+# The sixth (6th) animal.
+puts "The 6th animal is at 5 and is 'platypus'"
+puts animals[5]
+# The animal at 4.
+puts "The animal at 4 is the 5th and is 'whale'"
+puts animals[4]
---
+++ Chap34/study_drills.rb
@@ -0,0 +1,11 @@
+# With what you know of the difference between these types of numbers, can you explain why the year 2010 in "January 1, 2010," really is
+# 2010 and not 2009? (Hint: you can't pick years at random.)
+
+# Because the georgian calendar doesn't start at the year zero?
+
+boroughs_of_london = ['Camden', 'Greenwich', 'Hackney', 'Hammersmith', 'Islington']
+
+puts "The 1st borough in the list is #{boroughs_of_london[0]}"
+puts "The last borough in the list is #{boroughs_of_london[4]}"
+puts "The third borough in the list is #{boroughs_of_london[2]}"
+puts "The second to last borough in the list is #{boroughs_of_london[3]}"
